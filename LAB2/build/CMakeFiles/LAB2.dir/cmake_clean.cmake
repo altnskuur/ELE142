185f5c1c3cbac043d24b2177c3831c606228ba3f
altnskuur <ualtinisik@etu.edu.tr>
2025-01-31 09:27:09 +0300

    LAB2 added

---
+++ LAB2/build/CMakeFiles/LAB2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAB2.dir/main.cpp.obj"
+  "CMakeFiles/LAB2.dir/main.cpp.obj.d"
+  "D:/altnskuur/TOBB/ELE 142/repos/LAB2/181201033_lab2.exe"
+  "D:/altnskuur/TOBB/ELE 142/repos/LAB2/181201033_lab2.exe.manifest"
+  "D:/altnskuur/TOBB/ELE 142/repos/LAB2/181201033_lab2.pdb"
+  "lib181201033_lab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LAB2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LAB2/main.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+
+void compare(int *first, int *second);
+void show(int dizi[], int boyut);
+
+int main(){
+
+    srand(time(NULL));
+    const int c_elemannumber = 100;
+    int *arr = new int(c_elemannumber);
+    
+    // fill random to the array dynamically
+    for(int i = 0; i < c_elemannumber; i++){
+
+        *(arr+i) = rand()%100;
+    }
+
+    // print elements of random array with weights
+    std::cout << "Random array: " << std::endl;
+    show(arr, c_elemannumber);
+
+    // buble sort algorithm with compare function
+    std::cout << "Bubble sorting running..." << std::endl;
+    for(int i = 0; i < (c_elemannumber-1); i++){
+
+        for(int j = 0; j < (c_elemannumber-1-i); j++){
+
+            compare((arr+j), (arr+j+1));
+            
+        }
+    }
+
+    // print elements of sorted array with weights
+    std::cout << "Sorted Array: " << std::endl;
+    show(arr, c_elemannumber);
+
+    delete arr;
+    return 0;
+}
+
+void compare(int *first, int *second){
+
+    if(*first > *second){
+
+        int temp = *first;
+        *first = *second;
+        *second = temp;
+    }
+}
+
+void show(int dizi[], int boyut){
+
+    for(int elem = 0; elem < boyut; elem++){
+
+        for(int weight = 0; weight < *(dizi+elem); weight++){
+
+            std::cout << "|";
+        }
+        std::cout << *(dizi+elem) << std::endl;
+    }
+    
+}
